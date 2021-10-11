4aa871ce2708bda0db27457a3066fb7517657bc3
Samuel Platon <samuel@binch-solutions.com>
2021-10-11 10:52:16 +0200

    init(lorenzo,samuel): setup classes

---
+++ src/classes/Player.py
@@ -0,0 +1,7 @@
+
+class Player:
+    def __init__(self, id, name, weight, experience):
+        self.id = id
+        self.name = name
+        self.weight = weight
+        self.experience = experience
