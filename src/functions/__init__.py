82c2c09f647616ecf9fe5c473f3add662e17ef58
Samuel Platon <samuel@binch-solutions.com>
2021-10-12 19:55:34 +0200

    refactor(lorenzo): automatic team generation

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -5,6 +5,5 @@ Description : Contains our functions and algorithms external to a class
 
 """
 
-from .determine_category import determine_category
-from .generate_teams import generate_teams
-
+#from .determine_category import determine_category
+from .generate_teams import generate_teams, determine_category
