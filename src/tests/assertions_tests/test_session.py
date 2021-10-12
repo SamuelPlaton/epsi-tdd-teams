8827f7917cb589afd1c79d2247b4266c50cce30a
Romain <46965933+romaingrimault@users.noreply.github.com>
2021-10-12 16:20:50 +0200

    feat/(romain) test_match_pending_players

--- src/tests/assertions_tests/test_session.py
+++ src/tests/assertions_tests/test_session.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 
 from classes import Session
+from ..helpers import compare_teams_weight
 
 """ 
 name : TestSession
@@ -10,14 +11,45 @@ description: Test the Generation of list
 class TestSession(unittest.TestCase):
     def setUp(self):
         self.session = Session()
+    """
+        
+    """
+    def test_match_pending_players (self):
+        # Retrieve our 9 players from csv then start our match
+        players = self.session.retrieve_players('./src/tests/fixtures/dataTest.csv')
+        self.session.prepare_players(players, [1, 2, 3, 4, 5, 6, 7, 8, 9,10])
 
-    @patch('classes.session.Session.greetings_input', return_value='o')
-    @patch('classes.session.Session.prepare_players_input', return_value='1,2,3,4,5')
-    @patch('classes.session.Session.process_game_input', return_value='1')
-    def test_match_irregular_add_player(self, input, input2, input3):
-        self.session.greetings()
-        self.session.prepare_players()
         self.session.match.start_game()
-        self.session.print_prompt()
-        # On vérifie les données de la session
-        self.assertEqual(len(self.session.match.first_team), len(self.session.match.second_team))
+        first_team_before=self.session.match.first_team.players
+        second_team_before=self.session.match.second_team.players
+        self.session.add_pending_player('EL BOBBY', 20)
+        self.session.add_pending_player('EL SECOND BOBBY', 30)
+        #look if team haven't change
+        self.assertEqual(len(first_team_before), len(self.session.match.first_team.players),
+                         "Teams should have the same amount of players")
+        self.assertEqual(len(second_team_before), len(self.session.match.second_team.players),
+                         "Teams should have the same amount of players")
+        self.session.match.end_game()
+        #Create a new match
+        self.session.prepare_players(players, [1, 2, 3, 4, 5, 6, 7, 8, 9,10])
+
+        self.session.match.start_game()
+        #Look if one of the two player have been added to the first team
+        find = False
+        for elem in self.session.match.first_team.players:
+            if elem.name == 'EL SECOND BOBBY' or elem.name == "EL BOBBY":
+                find = True
+        self.assertTrue(find,'EL Booby sould be here')
+        #Look if one of the two player have been added to the second team
+
+        find = False
+        for elem in self.session.match.second_team.players:
+            if elem.name == "EL SECOND BOBBY" or elem.name == "EL BOBBY":
+                find = True
+        self.assertTrue(find,'EL  Booby sould be here')
+        self.assertNotEqual(len(first_team_before), len(self.session.match.first_team.players),
+                         "Teams should have the same amount of players")
+        self.assertNotEqual(len(second_team_before), len(self.session.match.second_team.players),
+                         "Previous team and current team should not have the same amount of players")
+        self.session.match.end_game()
+        self.session.end_game()
