e2bd054173a7fd7d8fdb4d053b59a9f3543d2484
Samuel Platon <samuel@binch-solutions.com>
2021-10-12 19:57:06 +0200

    feat(romain): retrieve players from csv file

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -7,4 +7,6 @@ Description : Contains our classes and enums
 from .category import Category
 from .player import Player
 from .team import Team
+from .generator import Generator
 from .match import Match, MatchStatus
+
