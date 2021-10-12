26d6162b5288e5597187b9bf21bca72256046596
Romain <46965933+romaingrimault@users.noreply.github.com>
2021-10-12 09:23:08 +0200

    feat(romain) generate player from csv

---
+++ src/classes/team.py
@@ -0,0 +1,20 @@
+"""
+Class: Team
+
+Description: Our class team.
+
+"""
+class Team:
+    def __init__(self, name):
+        """ constructor
+        Setup our team.
+        """
+        self.name = name
+        self.players = []
+
+    def append_player(self, player):
+        """ append_player
+        append a player to the team.
+        :parameter player   The player to add to the team
+        """
+        self.players.append(player)
